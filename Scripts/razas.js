d3adbffc7d2cb6c7f89cffa5bf98de3677cb1275
KatherineDeLaCruz <keldlc282@gmail.com>
2024-10-31 22:00:43 -0600

    header y footer + boton

--- Scripts/razas.js
+++ Scripts/razas.js
@@ -76,3 +76,32 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+// botón hacia arriba
+
+const upBtn = document.getElementById("upBtn");
+
+window.onscroll = function () { handleScroll() };
+
+function handleScroll() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        upBtn.classList.remove("hide");
+        upBtn.classList.add("show");
+        upBtn.style.display = "block";
+    } else {
+        upBtn.classList.remove("show");
+        upBtn.classList.add("hide");
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollTopFd() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
